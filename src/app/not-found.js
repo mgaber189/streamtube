d58e9ee1d6b7ef798f4881ed9a66e153fe88fe5b
unknown <mg3845617@gmail.com>
2025-05-01 06:42:14 +0300

    fix vercal build error

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen  text-white px-6">
+    <div className="flex flex-col items-center justify-center min-h-screen text-white px-6">
       <h2 className="text-5xl font-bold mb-4 text-red-500">404 - Not Found</h2>
       <p className="text-lg mb-8 text-gray-300">
-        Sorry, the page you're looking for doesn't exist.
+        Sorry, the page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link
         href="/"
